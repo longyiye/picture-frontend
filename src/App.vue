e42dfb351a5734954ada51b8d6724af9c2f360e6
龙一野 <2226548812@qq.com>
2025-09-18 23:23:48 +0800

    前端项目初始化

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="app">
+    <BasicLayout />
+  </div>
+</template>
+
+<script setup lang="ts">
+import BasicLayout from '@/layouts/BasicLayout.vue'
+import { testUsingGet } from '@/api/testController'
+import { useLoginUserStore } from '@/stores/useLoginUserStore'
+
+const loginUserStore = useLoginUserStore()
+loginUserStore.fetchLoginUser()
+
+testUsingGet().then((res) => {
+  console.log(res)
+})
+</script>
+
+<style>
+#app {
+}
+</style>
---
+++ src/pages/HomePage.vue
@@ -0,0 +1,14 @@
+<template>
+  <div id="homePage">
+    <h1>{{ msg }}</h1>
+  </div>
+</template>
+
+<script setup lang="ts">
+const msg = '欢迎来到 AI 彩虹云穹，你将从这里获得意想不到的惊喜'
+</script>
+
+<style scoped>
+#homePage {
+}
+</style>
