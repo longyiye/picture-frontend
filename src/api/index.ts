f7f7cba070080587491e229b1586225757c20695
龙一野 <2226548812@qq.com>
2025-09-29 15:43:12 +0800

    开发图片上传页面，图片管理页面，图片列表页面，图片详情页面

--- src/api/index.ts
+++ src/api/index.ts
@@ -2,9 +2,13 @@
 /* eslint-disable */
 // API 更新时间：
 // API 唯一标识：
+import * as fileController from './fileController'
+import * as pictureController from './pictureController'
 import * as testController from './testController'
 import * as userController from './userController'
 export default {
+  fileController,
+  pictureController,
   testController,
   userController,
 }
